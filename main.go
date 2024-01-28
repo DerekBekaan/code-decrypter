5a82a068626d85a0958206519e3b8aca8630cf7b
Derek <derekbekaan@gmail.com>
2024-01-28 10:54:07 -0500

    can run simulation of many games

--- main.go
+++ main.go
@@ -4,32 +4,60 @@ import (
 	"code-decrypter/code"
 	"code-decrypter/game"
 	"fmt"
+	"math/rand"
 )
 
 func main() {
+	numberOfGames := 10000
+
+	maxGuesses := 0
+	avgGuesses := make([]int, numberOfGames)
 	possibleCodes := code.GenerateAllCodes()
 
-	game := game.Game{CodeToGuess: code.Code{6, 1, 4, 3, 5}}
-	count := 1
+	for gi := 0; gi < numberOfGames; gi++ {
+		codeIndex := rand.Intn(len(possibleCodes) - 1)
+		game := game.Game{CodeToGuess: possibleCodes[codeIndex]}
+		count := 1
+		firstBadCode := len(possibleCodes)
 
-	for {
-		guess := possibleCodes[len(possibleCodes)-1]
-		result := guess.CompareTo(game.CodeToGuess)
-		fmt.Printf("Guess %v: %v\n", count, guess)
+		for {
+			guess := possibleCodes[0]
+			result := guess.CompareTo(game.CodeToGuess)
 
-		if result.Correct == len(game.CodeToGuess) {
-			fmt.Printf("Correct %v", guess)
-			break
-		}
-
-		newPossibleCodes := make([]code.Code, 1)
-		for i := 0; i < len(possibleCodes); i++ {
-			if possibleCodes[i].CompareTo(guess) == result {
-				newPossibleCodes = append(newPossibleCodes, possibleCodes[i])
+			if result.Correct == len(game.CodeToGuess) {
+				if count > maxGuesses {
+					maxGuesses = count
+				}
+				avgGuesses[gi] = count
+				break
 			}
-		}
 
-		possibleCodes = newPossibleCodes
-		count++
+			// front load the good codes
+			for i := 0; i < firstBadCode; i++ {
+				if possibleCodes[i].CompareTo(guess) != result {
+					// swap the first good code
+					temp := possibleCodes[i]
+					possibleCodes[i] = possibleCodes[firstBadCode-1]
+					possibleCodes[firstBadCode-1] = temp
+					firstBadCode--
+				}
+
+				// trim the end to avoid swapping
+				for possibleCodes[firstBadCode-1].CompareTo(guess) != result {
+					firstBadCode--
+				}
+			}
+
+			count++
+		}
 	}
+
+	var sum int
+	for i := 0; i < len(avgGuesses); i++ {
+		sum += avgGuesses[i]
+	}
+	average := float32(sum) / float32(len(avgGuesses))
+
+	fmt.Printf("Max Guesses: %v\n", maxGuesses)
+	fmt.Printf("Avg Guesses: %.2f\n", average)
 }
